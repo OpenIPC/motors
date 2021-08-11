af40f9b2a062bab109df606c9b73c454a83e4bcc
Dmitry Ilyin <dima@doty.ru>
2021-08-11 16:28:03 +0300

    Dump received data from UART

--- XM/main.c
+++ XM/main.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/poll.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -18,6 +19,36 @@ static void send_cmd(int fd, const char *name, const unsigned char *cmd) {
 #define CMD(name, data)                                                        \
   send_cmd(uart, name, data);                                                  \
   break;
+
+static void DumpHex(const void *data, size_t size) {
+  char ascii[17];
+  size_t i, j;
+  ascii[16] = '\0';
+  for (i = 0; i < size; ++i) {
+    printf("%02X ", ((unsigned char *)data)[i]);
+    if (((unsigned char *)data)[i] >= ' ' &&
+        ((unsigned char *)data)[i] <= '~') {
+      ascii[i % 16] = ((unsigned char *)data)[i];
+    } else {
+      ascii[i % 16] = '.';
+    }
+    if ((i + 1) % 8 == 0 || i + 1 == size) {
+      printf(" ");
+      if ((i + 1) % 16 == 0) {
+        printf("|  %s \n", ascii);
+      } else if (i + 1 == size) {
+        ascii[(i + 1) % 16] = '\0';
+        if ((i + 1) % 16 <= 8) {
+          printf(" ");
+        }
+        for (j = (i + 1) % 16; j < 16; ++j) {
+          printf("   ");
+        }
+        printf("|  %s \n", ascii);
+      }
+    }
+  }
+}
 
 int main() {
   struct termios ctrl;
@@ -54,26 +85,57 @@ int main() {
   cfmakeraw(&options);
   tcsetattr(uart, TCSANOW, &options);
 
+  int flags = fcntl(uart, F_GETFL, 0);
+  fcntl(uart, F_SETFL, flags | O_NONBLOCK);
+
   printf("Xingongmai Motors, get in a car and fasten your safety belt\n");
-  printf("Commands: +/-\n");
+  printf("Commands: + - Enter (to cancel)\n");
 
-  unsigned char rbuf[1024];
-  char ch;
+  while (1) {
 
-  while (read(STDIN_FILENO, &ch, 1)) {
+    struct pollfd pfds[2] = {
+        {.fd = 0, .events = POLLIN},
+        {.fd = uart, .events = POLLIN},
+    };
 
-    switch (ch) {
-    case '-':
-      CMD("Zoom out", zoom_out);
+    poll(pfds, 2, -1);
 
-    case '+':
-      CMD("Zoom in", zoom_in);
+    if (pfds[0].revents & POLLIN) {
+      char ch;
+      int i = read(0, &ch, 1);
 
-    case '\n':
-      CMD("Cancel", cancel);
+      if (!i) {
 
-    default:
-      printf("Unknown command %c\n", ch);
+        printf("stdin closed\n");
+        return 0;
+      }
+      switch (ch) {
+      case '-':
+        CMD("Zoom out", zoom_out);
+
+      case '+':
+        CMD("Zoom in", zoom_in);
+
+      case '\n':
+        CMD("Cancel", cancel);
+
+      default:
+        printf("Unknown command %c\n", ch);
+      }
+    }
+
+    if (pfds[1].revents & POLLIN) {
+      unsigned char rbuf[1024];
+
+      int i = read(uart, rbuf, sizeof(rbuf));
+
+      if (!i) {
+        printf("UART closed\n");
+        return 0;
+      }
+
+      if (i != -1)
+        DumpHex(rbuf, i);
     }
   }
   close(uart);
